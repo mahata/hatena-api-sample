5a6a86a37416d6ff96784208cfc01b238b92a0d6
Yasunori Mahata <mahata777@gmail.com>
2014-05-05 01:32:44 -0700

    Prepare for API request.

--- project/Build.scala
+++ project/Build.scala
@@ -3,19 +3,16 @@ import Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
-
   val appName         = "hatena-api-sample"
   val appVersion      = "1.0-SNAPSHOT"
-
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "org.apache.abdera" % "abdera-bundle" % "1.1.3"
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
   )
-
 }
